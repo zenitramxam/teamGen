ac9690d69c1b799987341fbf7c3663c9fc3339c0
Max <m412tinez@gmail.com>
2021-07-06 14:54:22 -0500

    working on employee test, modified package json to test using jest

---
+++ tests/employee.test.js
@@ -0,0 +1,36 @@
+const Employee= require('../lib/employee');
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should return an object", () => {
+            const obj= new Employee();
+
+            expect(typeof(obj)).toEqual("object");
+        })
+    })
+
+    describe("Name", () => {
+        it("should return employee name", () => {
+            const obj= new Employee("Max");
+            
+            expect(obj.name).toEqual("Max");
+        })
+    })
+
+    describe("Id", () => {
+        it("should assign employee id", () => {
+            const obj= new Employee("Max", 1);
+            
+            expect(obj.id).toEqual(1);
+        })
+    })
+
+    describe("Email", () => {
+        it("should return employee email", () => {
+            const obj= new Employee("Max", 1, "max@email.com");
+            
+            expect(obj.email).toEqual("max@email.com");
+        })
+    })
+
+});

5c4d09d8ce27a6d0586b8c8274fe8dbd04fd6982
Max <m412tinez@gmail.com>
2021-07-06 17:55:14 -0500

    completed questions

--- index.js
+++ index.js
@@ -57,12 +57,70 @@ const managerQuestions= () => {
     inquirer
         .prompt ([
             {
-                type: "input",
-                name: "name",
-                message: "What is your name?",
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
             },
             {
-                
-            }
+                type: 'input',
+                name: 'id',
+                message: 'What is your ID number?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your Email?',
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: 'What is your office number?',
+            },
         ])
-}
+        .then ((answers) => {
+            const newManager= new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.office,
+            );
+            managers.push(newManager);
+            addTeamMember();
+        });
+};
+
+const engineerQuestions= () => {
+    inquirer
+        .prompt ([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your ID number?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your Email?',
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is your github username?',
+            },
+        ])
+        .then ((answers) => {
+            const newEngineer= new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.github,
+            );
+            engineers.push(newEngineer);
+            addTeamMember();
+        });
+};
